1732df24b020b40a50a9e03dc950de4d80d8879d
alexisaxon <70723693+alexisaxon@users.noreply.github.com>
2021-01-12 22:31:38 -0500

    Added driver.java

---
+++ driver.java
@@ -0,0 +1,8 @@
+public class driver {
+
+    public static void main(String[] args) {
+        Quiz q = new Quiz("Political Ideology Test","People make too big of a deal out of climate change");
+        q.question("People make too big of a deal out of climate change",true,false);
+
+    }
+}
